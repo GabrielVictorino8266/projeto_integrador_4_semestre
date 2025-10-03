24c7fecd5a3d108cc2709d8668472ce053c4ffe7
bruno.santos <bruno.santos382@fatec.sp.gov.br>
2025-10-03 15:57:52 -0300

    COF-15: Padronizar respostas e tratamento de erros na API  - Padronizar payloads de resposta para melhorar clareza e previsibilidade  - Tratar erros de negócio com mensagens amigáveis (ex.: veículo duplicado)

---
+++ src/main/java/com/projetoanderson/app/exception/VeiculoDuplicadoException.java
@@ -0,0 +1,7 @@
+package com.projetoanderson.app.exception;
+
+public class VeiculoDuplicadoException extends RuntimeException {
+    public VeiculoDuplicadoException(String message) {
+        super(message);
+    }
+}
